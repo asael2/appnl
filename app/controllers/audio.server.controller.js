4536aaed9d6443789560ff53f5a5d3c02ad28e30
ayf media <asael2@gmail.com>
2014-07-10 23:06:29 -0500

    working in audio article

---
+++ app/controllers/audio.server.controller.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    _ = require('lodash');
+
+/**
+ * Create a Audio
+ */
+exports.create = function(req, res) {
+
+};
+
+/**
+ * Show the current Audio
+ */
+exports.read = function(req, res) {
+
+};
+
+/**
+ * Update a Audio
+ */
+exports.update = function(req, res) {
+
+};
+
+/**
+ * Delete an Audio
+ */
+exports.delete = function(req, res) {
+
+};
+
+/**
+ * List of Audios
+ */
+exports.list = function(req, res) {
+
+};
--- public/modules/elearning/config/elearning.client.routes.js
+++ public/modules/elearning/config/elearning.client.routes.js
@@ -5,8 +5,16 @@ angular.module('elearning').config(['$stateProvider',
     function($stateProvider) {
         // Elearning state routing
         $stateProvider.
+        state('audio', {
+            url: '/audio/:articleId',
+            templateUrl: 'modules/elearning/views/audio.client.view.html'
+        }).
+        state('article-video', {
+            url: '/video/:articleId',
+            templateUrl: 'modules/elearning/views/article-video.client.view.html'
+        }).
         state('learnarticle', {
-            url: '/learnarticle/:articleId',
+            url: '/texto/:articleId',
             templateUrl: 'modules/elearning/views/learnarticle.client.view.html'
         }).
         state('learnroute', {
