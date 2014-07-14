6c18bc4a6cb6aeb946228379d60d479c57178618
ayf media <asael2@gmail.com>
2014-07-14 02:50:08 -0500

    adding well lets go for myView

--- public/modules/articles/controllers/my-articles.client.controller.js
+++ public/modules/articles/controllers/my-articles.client.controller.js
@@ -4,58 +4,17 @@ angular.module('users').controller('MyArticlesController', ['$scope', 'Articles'
     function($scope, Articles, Users, User) {
 
         $scope.addArt2me = function(req, res) {
-
             var myArticle = new Articles($scope.article);
 
             $scope.myUser.userArticles.push(myArticle._id);
-
             console.log("Mis articulos: " + $scope.myUser.userArticles.length);
 
             $scope.myUser.$update(function(response) {
                 console.log("Actualize!! con : " + $scope.myUser.userArticles);
             }, function(errorResponse) {
-                console.log("updatError: " + myArticle._id + errorResponse);
+                console.log("updatError: " + myArticle._id + errorResponse.data);
                 $scope.error = errorResponse;
             });
-
-
-            // myUser.$update({
-            //     userArticles: artid
-            // })
-            // console.log(user.userArticles.length);
-
-            // myUser.findByIdAndUpdate(user, {
-            //     userArticles: userArticles
-            // }, function(err, req) {
-            //     if (!err) console.log(">>Articulo Actualizado.");
-            //     else console.log('Error:' + err);
-            // });
         };
-
-        // myUser.push({
-        //     userArticles: myArticle
-        // }).update({
-        // _id: myUser
-        // });
-
-
-
     }
 ]);
-
-
-// console.log($scope.article._id);
-
-
-
-
-//     $update(user.userArticles, myArticle._id);
-//     console.log("userArticles: " + user.userArticles.length);
-//     // myUser.$update(function(response) {
-//     //     console.log("Actualize!! con : " + user.userArticles.length + "__" + response);
-//     // }, function(errorResponse) {
-//     //     console.log("updatError: " + myArticle._id + errorResponse);
-//     //     $scope.error = errorResponse;
-//     // });
-
-// }
